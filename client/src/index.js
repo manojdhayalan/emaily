0fd767e2ef8e08c66440121531f513effbbd866e
Manojkumar Dhayalan <manojkumar@Manojkumars-MacBook-Pro.local>
2025-07-26 23:59:35 +0530

    Fixed build issue

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from "redux";
 import reduxThunk from "redux-thunk";
 import reducers from './reducers';
-import App from "./components/App";
+import App from "./components/App.js";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
